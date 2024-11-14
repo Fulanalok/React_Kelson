2f09db2544743acc99f88a0a795a70cb079f436f
Lucas Vilhena <66475011+Fulanalok@users.noreply.github.com>
2024-11-14 11:47:20 -0300

    Add files via upload

---
+++ Aula01/src/components/Divisao.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Divisao = (props) => {
+  return (
+    <div>
+      <h1> o resultado de num3 / num4 é:
+        {props.num3/props.num4}
+        </h1>
+    </div>
+  )
+}
+
+export default Divisao
---
+++ aula02/src/components/TrabalhandoComImagens.jsx
@@ -0,0 +1,13 @@
+import React from 'react' 
+import NightmareImg from '../assets/nightmare.jpeg'
+
+const TrabalhandoComImagens = () => {
+  return (
+    <div>
+        <img src='megadeth.jpeg' />
+        <img src={NightmareImg} />
+    </div>
+  )
+}
+
+export default TrabalhandoComImagens
