a112e22ad2ef5480ef7cbe470fe9c051a58bf6dc
Lucas Vilhena <66475011+Fulanalok@users.noreply.github.com>
2024-11-21 12:33:52 -0300

    Update multiplicacao.jsx

--- Aula01/src/components/multiplicacao.jsx
+++ Aula01/src/components/multiplicacao.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Multiplicacao = () => {
   return (
     <div>
-        <h1> resultado do num1 * num2 e:
+        <h1> resultado do num1 * num2 e: </h1>
         <Multiplicacao/></div>
   )
 }
